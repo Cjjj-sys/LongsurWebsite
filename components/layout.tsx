5c2322ffab4b2659d38361515940bfb6bdc7a741
KangKang <44329867+Cjjj-sys@users.noreply.github.com>
2023-01-24 07:10:59 +0000

    fix

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,25 +5,13 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Longsur - 我的世界基岩版服务器';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'longsur服务器官方网站';
 
 const Layout = ({ children, home }: any) => {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
         {home ? (
